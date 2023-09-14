3fd1d4266ba170919868e3e51fc87f6225acd6e4
NaviteLogger <kacprzakmarek92@gmail.com>
2023-09-14 17:03:45 +0200

    Added first code: sample reaction to user's '\!ping' message

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Discord-utility-and-music-bot
-Discord utility, server management and music bot
+# Discord-music-bot
+Discord music bot
 Feel free to PR if you have ideas how to improve this bot!
---
+++ bot.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!ping') {
+        message.channel.send('Pong!');
+    }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
