f614732bbda18154c8933f75bd6df029ffebb4a9
NaviteLogger <kacprzakmarek92@gmail.com>
2023-09-14 18:34:14 +0200

    Removed the temporary startup message

--- bot.js
+++ bot.js
@@ -3,14 +3,6 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client({ intents: ['Guilds', 'GuildMessages'] });
 
-client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    const testChannel = client.channels.cache.get('1151896705583816767');
-    if (testChannel) {
-        testChannel.send('Bot has started up');
-    }
-});
-
 client.on('messageCreate', message => {
     console.log(`Received message: ${message.content} from ${message.author.tag}`); // Log received messages
     if (message.content === '!ping') {
