3a36bf72483739c1565d48b4b850613ad1644b25
NaviteLogger <kacprzakmarek92@gmail.com>
2023-09-16 13:35:28 +0200

    Removed the test message interaction '!ping'

--- bot.js
+++ bot.js
@@ -10,14 +10,7 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', message => {
-    console.log(`Received message: "${message.content}" from ${message.author.tag}`); // Log received messages
-    if (message.content === "!test")
-    {
-        console.log('Ping command received'); // Log when the ping command is received
-        message.channel.send('Ye ye I am working fine')
-            .then(msg => console.log('Sent reply: ', msg.content)) // Log if the message is sent successfully
-            .catch(error => console.error('Failed to send message: ', error)); // Log if there is an error sending the message
-    }
+    
 });
 
 client.on('debug', console.log);
