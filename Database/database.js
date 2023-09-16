893077dc189b6424edfd50b57402bfa8e358c076
NaviteLogger <kacprzakmarek92@gmail.com>
2023-09-16 14:35:47 +0200

    Added a fix for the wrong name of the db table

--- Database/database.js
+++ Database/database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 
 //Opening of the database handle
-let db = new sqlite3.Database('./db/quotes.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (error) => {
+let db = new sqlite3.Database('./Database/botDatabase.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (error) => {
     if (error) {
         console.error(error.message);
     }
