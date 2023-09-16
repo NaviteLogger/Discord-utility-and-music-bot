fc77031114cdd5fe5a35857a348a24bbd6828e9d
NaviteLogger <kacprzakmarek92@gmail.com>
2023-09-16 13:43:37 +0200

    Added the if statement that will ignore the non-human messages (if(message.guild))

--- bot.js
+++ bot.js
@@ -9,8 +9,22 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', message => {
-    
+client.on('messageCreate', async message => {
+    //Ignore messages from bots - basically read/react only to human messages
+    if(message.guild)
+    {
+        let prefix = await getPrefix(message.guild.id);
+        if(!prefix)
+        {
+            prefix = '!'; //Set the prefix to the default one
+        }
+
+        //Ignore messages that don't start with the prefix
+        if(message.content.startsWith(prefix))
+        {
+            const args = message.content.slice(prefix.length).trim().split(/ +/); //Split the message into an array of arguments
+        }
+    }
 });
 
 client.on('debug', console.log);
