7ed8ac0c273e3ff3a3b6a4b0793fbfa91e3da4ab
NaviteLogger <kacprzakmarek92@gmail.com>
2023-09-16 13:29:30 +0200

    Added the getPrefix funtion to retrieve the prefix of the server using the guildId

--- Database/database.js
+++ Database/database.js
@@ -13,5 +13,24 @@ db.serialize(() => {
     db.run("CREATE TABLE IF NOT EXISTS guilds (guild_id TEXT, prefix TEXT)");
 });
 
+//This query is used to get the prefix of a guild
+function getPrefix(guildIdd)
+{
+    return new Promise((resolve, reject) => {
+        db.get("SELECT prefix FROM guilds WHERE guild_id = ?", [guildId], (error, row) => {
+            if (error)
+            {
+                console.error(error.message);
+                reject(error);
+            }
+                else
+            {
+                console.log(`Prefix of guild ${guildId} is ${row.prefix}`);
+                resolve(row.prefix);
+            }
+        });
+    });
+}
+
 //Exporting the database handle
 module.exports = db;
